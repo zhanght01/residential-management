ef6a1bc12c33ea20cf46b7711c5f91099ce0a75b
zhanght <zhang@zhanghaitao>
2017-04-13 13:54:28 +0800

    js写到静态文件中

---
+++ src/main/webapp/js/login.js
@@ -0,0 +1,17 @@
+function login() {
+            var userName = $("#userName").val();
+            var password = $("#password").val();
+            var roleName = $("#roleName").val();
+            if (userName == null || userName == "") {
+                alert("用户名不能为空！");
+                return;
+            }
+            if (password == null || password == "") {
+                alert("密码不能为空！");
+                return;
+            }
+            $("#adminlogin").submit();
+
+        }
+
+
